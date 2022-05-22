d357de98b5310d4fe138c66455850ba8c4671d67
EVReborns <49639007+EVReborns@users.noreply.github.com>
2022-05-22 18:19:51 +0800

    First Commit
    1. Added function in main.js
    2. JS Function from main.js run in index.html
    3. Top header stays on the top

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,12 @@
 // Task: Implement isNameInList function
 
-var names = [
-  'jax',
-  'tryndamere',
-  'anivia',
-  'ahri'
-];
-
-function isNameInList (name) {
-  throw new Error('isNameInList function not implemented');
+function isNameInList (names) {
+  var names = ["jax","tryndamere","anivia","ahri"];
+  for (let i=0; i<isNameInList.length; i++)
+  if(isNameInList[i]==='pikachu'){
+    return alert("Correct, Pikachu is a League of Legends Champion");
+  }
+  else{
+    return alert("False, Pikachu is not a Leaguer of legends Champion");
+  }
 }
-
-console.log('Is Pikachu a league of legends champion? ', isNameInList('pikachu'));
